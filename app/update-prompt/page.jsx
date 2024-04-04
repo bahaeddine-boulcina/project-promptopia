fb0db4078359ae9266e0d434f7681f839bed3421
bahaeddine-boulcina <bahaeddine.boulcina@univ-constantine2.dz>
2024-04-04 12:04:52 +0100

    fix update-prompt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { Suspense, useEffect, useState } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
 
 import Form from "@components/Form"
@@ -54,6 +54,7 @@ const EditPrompt = () => {
   }
 
   return (
+    <Suspense>
     <Form
       type="Edit"
       post={post}
@@ -61,6 +62,7 @@ const EditPrompt = () => {
       submitting={submitting}
       handleSubmit={updatePrompt}
     />
+    </Suspense>
   )
 }
 
